368eefb83c86994cc6fd7ba5bb4dba4429552738
SiwooPak <bogus1620@naver.com>
2022-02-28 22:14:08 +0900

    Add: iterable map()

---
+++ map_filter_reduce/filter.js
@@ -0,0 +1,11 @@
+const log = console.log;
+
+const products = [
+  { name: "half_tee", price: 15000 },
+  { name: "long_tee", price: 20000 },
+  { name: "mobile_case", price: 15000 },
+  { name: "hood_tee", price: 30000 },
+  { name: "pants", price: 25000 },
+];
+
+/* filter */
