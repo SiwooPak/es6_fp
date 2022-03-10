6396bab4aa642728cf71b097e1d35d8d1cdbf22b
SiwooPak <bogus1620@naver.com>
2022-03-11 00:16:10 +0900

    Modify: fx.js

--- 지연성/lazy_evaluation2.js
+++ 지연성/lazy_evaluation2.js
@@ -1,18 +1,31 @@
-const { _filter, _take, _map, _map, log, L } = require("../lib/fx");
+const {
+  _filter,
+  _take,
+  _map,
+  _range,
+  _go,
+  log,
+  L,
+} = require("../lib/fx");
 
 // range, map, filter, take, reduce 중첩사용
-go(
+console.time("no lazy");
+_go(
   _range(10),
   _map((n) => n + 10),
   _filter((n) => n % 2),
   _take(2),
   log
 );
+console.timeEnd("no lazy");
+
 // 위의 코드 지연평가로 리팩토링
-go(
+console.time("lazy func");
+_go(
   L._range(10),
   L._map((n) => n + 10),
   L._filter((n) => n % 2),
-  L._take(2),
+  _take(2),
   log
 );
+console.timeEnd("lazy func");
