11eb35039ddbae324b05085b7576486c072998e3
SiwooPak <bogus1620@naver.com>
2022-03-12 23:44:28 +0900

    Add: map, filter 결합법칙

--- vending_machine.js
+++ vending_machine.js
@@ -1,15 +1,18 @@
 const { _go, _filter, _reduce } = require("./lib/fx");
 const { log, table } = console;
-const check_zero = (products) => products.length > 0;
+
+const _isObject = (obj) => typeof obj === "object" && !!obj;
+const _keys = (obj) => (_isObject(obj) ? Object.keys(obj) : []);
 const changeQty = (products, no) => {
   products.forEach((p) => {
     if (p.no === no) return p.qty--;
   });
 };
-const total = (avlCash) =>
+
+const total = (obj) =>
   _go(
-    Object.keys(avlCash),
-    _reduce((a, b) => Number(a) * avlCash[a] + Number(b) * avlCash[b])
+    _keys(obj),
+    _reduce((a, b) => Number(a) * obj[a] + Number(b) * obj[b])
   );
 
 const last_changes = (totalCash, changes) => {
@@ -28,10 +31,12 @@ const productsStatus = (str, products) => {
   log("================");
 };
 
+const _head = (list) => list[0];
+
 const products = [
   { no: 1, name: "cola", price: 1100, qty: 2 },
   { no: 2, name: "water", price: 600, qty: 5 },
-  { no: 3, name: "coffee", price: 700, qty: 5 },
+  { no: 3, name: "coffee", price: 700, qty: 0 },
 ];
 
 const coins = [10000, 5000, 1000, 500, 100];
@@ -40,7 +45,7 @@ const coins = [10000, 5000, 1000, 500, 100];
   - selected_product: 선택한 상품의 번호
   - payment: 결제수단 방식
   - cash: 현금 결제식 투입되는 현금
-  - selectedProductIno: 선택한 상품의 정보
+  - getSelectedProductIno: 선택한 상품의 정보
   - changes: 잔돈
   - result: 결과값
 */
@@ -50,35 +55,31 @@ const main = (selectedProduct, payment, cash) => {
   cash = cash || 0;
   // 결과값
   let result = {};
-  productsStatus("구매 전 재고 상태", products);
+
+  // productsStatus("구매 전 재고 상태", products);
   // 결제수단이 혐금이고 현금을 투입하지 않을시
   if (payment === "cash" && cash === 0) {
     return { screen: "금액부족" };
   }
 
-  // 선택한 상품 재고 확인
-  const productStockCheck = _go(
+  // 선택한 상품의 정보 가져오기
+  const getSelectedProductInfo = _go(
     products,
-    _filter((p) => p.qty > 0 && p.no === selectedProduct),
-    check_zero
+    _filter((p) => p.no === selectedProduct),
+    _head
   );
 
-  // 선택한 상품의 정보 가져오기
-  const selectedProductInfo = _go(
-    products,
-    _filter((p) => p.no === selectedProduct)
-  )[0];
   // 번호에 해당하는 상품이 없는 경우
-  if (selectedProductInfo === undefined)
+  if (!getSelectedProductInfo)
     return {
       screen: `번호(${selectedProduct})에 해당하는 상품이 없습니다.`,
     };
 
   // 선택한 상품이 품절이라 구매 불가능한 경우
-  if (!productStockCheck) {
-    purchaseStatus = false;
-    result.screen = `선택하신 상품(${selectedProductInfo.name})은 품절입니다.`;
-    return result;
+  if (!getSelectedProductInfo.qty) {
+    return {
+      screen: `선택하신 상품 ${getSelectedProductInfo.name}는(은) 품절입니다.`,
+    };
   }
 
   // 구매가능한 경우, 결제수단은 카드인가?
@@ -86,18 +87,18 @@ const main = (selectedProduct, payment, cash) => {
     // 해당 상품의 재고 변경
     changeQty(products, selectedProduct);
     // 결과 반환(상품 출고)
-    result["selectedProduct"] = selectedProductInfo.name;
-    result["payment"] = payment;
+    result.output = getSelectedProductInfo.name;
+    result.payment = payment;
     // 만약 현금을 투입한 경우
-    if (cash > 0) result["changes"] = cash;
-    productsStatus("구매 전 재고 상태", products);
+    if (cash > 0) result.changes = cash;
+    //productsStatus("구매 후 재고 상태", products);
     return result;
   }
 
-  // 결제수단이 현금인 경우
+  // 구매가능하고, 결제수단이 현금인 경우
   // 투입한 현금 중에 사용가능한 현금만 걸러내기
   _go(
-    Object.keys(cash),
+    _keys(cash),
     _filter((c) => {
       if (coins.find((v) => v === Number(c))) {
         avlCash[c] = cash[c];
@@ -110,14 +111,14 @@ const main = (selectedProduct, payment, cash) => {
   let totalCash = total(avlCash);
 
   // 상품 금액이 충족한 경우
-  if (selectedProductInfo.price <= totalCash) {
+  if (getSelectedProductInfo.price <= totalCash) {
     // 해당 상품의 재고 변경
     changeQty(products, selectedProduct);
     // 현금 차감
-    totalCash -= selectedProductInfo.price;
+    totalCash -= getSelectedProductInfo.price;
     // 최종 거스름돈 구하기
     last_changes(totalCash, changes);
-    result.selectedProduct = selectedProductInfo.name;
+    result.output = getSelectedProductInfo.name;
     result.payment = payment;
     result.changes = changes;
   } else {
@@ -126,11 +127,15 @@ const main = (selectedProduct, payment, cash) => {
     result.screen = "금액부족";
     result.changes = changes;
   }
-  productsStatus("구매 후 재고 상태", products);
+  // productsStatus("구매 후 재고 상태", products);
 
   return result;
 };
 
+// 결제수단: 현금, 구매가능
 log(main(1, "cash", { 500: 2, 1000: 1 }));
 log(main(2, "cash", { 500: 2, 1000: 1 }));
+// 상품의 없는 번호인 경우
 log(main(4, "card"));
+// 품절이 상품
+log(main(3, "card"));
