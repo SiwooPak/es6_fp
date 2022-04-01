60acec6a37a05d0bca1f500aa99f33e6c9e29d4a
SiwooPak <bogus1620@naver.com>
2022-04-02 01:04:43 +0900

    Fix: 병렬적으로 즉시 평가 코드 수정 및 함수 추가 - 코드 함수명 수정 - 병렬적으로 처리하게 C._takeAll, C._map, C._filter 추가

--- 비동기_동시성프로그래밍2/instantly_parallel_eval.js
+++ 비동기_동시성프로그래밍2/instantly_parallel_eval.js
@@ -1,8 +1,27 @@
-const { C } = require("../lib/fx");
+const {
+  C,
+  L,
+  _curry,
+  _pipe,
+  log,
+  _reduce,
+  _go,
+  add,
+} = require("../lib/fx");
 
 // 즉시 병렬적으로 평가하기
-//C.map((a) => delay1000(a * a), [1, 2, 3, 4]).then(log);
-//C.filter((a) => delay1000(a % 2), [1, 2, 3, 4]).then(log);
+C._takeAll = C._take(Infinity);
+C._map = _curry(_pipe(L._map, C._takeAll));
+C._filter = _curry(_pipe(L._filter, C._takeAll));
+
+const delay1000 = (a) =>
+  new Promise((resolve) => {
+    log(a);
+    setTimeout(() => resolve(a), 1000);
+  });
+
+C._map((a) => delay1000(a * a), [1, 2, 3, 4]).then(log);
+C._filter((a) => delay1000(a % 2), [1, 2, 3, 4]).then(log);
 
 //  즉시, 지연, Promise, 병렬적 조합하기
 
@@ -14,13 +33,13 @@ const delay500 = (a, name) =>
   });
 
 console.time("");
-go(
+_go(
   [1, 2, 3, 4, 5, 6, 7, 8],
-  L.map((a) => delay500(a * a, "map 1")),
-  L.filter((a) => delay500(a % 2, "filter 2")),
-  L.map((a) => delay500(a + 1, "map 3")),
-  C.take(2),
-  reduce(add),
+  L._map((a) => delay500(a * a, "map 1")),
+  L._filter((a) => delay500(a % 2, "filter 2")),
+  L._map((a) => delay500(a + 1, "map 3")),
+  C._take(2),
+  _reduce(add),
   log,
   (_) => console.timeEnd("")
 );
