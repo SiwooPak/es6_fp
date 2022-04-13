7e325e0b7a5edd4943c73fa381bff6bb94494406
siwoopak <bogus1620@naver.com>
2022-04-13 23:45:34 +0900

    Add: 객체를 이터러블 프로그래밍으로 다루기 - entries(): 객체를 키와 값을 배열로

--- 함수형프로그래밍의응용/fp_app09.js
+++ 함수형프로그래밍의응용/fp_app09.js
@@ -1,4 +1,4 @@
-const { _go, _reduce, add, log, L, _take } = require('../lib/fx');
+const { _go, _reduce, add, log, L, _take, _takeAll } = require('../lib/fx');
 // 객체를 이터러블 프로그래밍으로 다루기
 const obj1 = {
   a: 1,
@@ -20,7 +20,18 @@ _go(
   log,
 );
 // 2. entries
-
+L._entries = function* (obj) {
+  for (const k in obj) yield [k, obj[k]];
+};
+_go(obj1, L._entries, _takeAll, log);
+_go(
+  obj1,
+  L._entries,
+  L._filter(([_, v]) => v % 2),
+  L._map(([k, v]) => ({ [k]: v })),
+  _reduce(Object.assign),
+  log,
+);
 // 1. keys
 
 // 1. 어떠한 값이든 이터러블 프로그래밍으로 다루기
